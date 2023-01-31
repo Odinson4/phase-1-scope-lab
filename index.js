4fdf4474790ea939490a8294db867c1ab56a72ba
Dylan Abbott <dylanabbott33@yahoo.com>
2023-01-31 08:36:48 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+}
+
+
+
+function setBestCustomer() {
+  return bestCustomer = customerName = "not bob";
+}
+
+function overwriteBestCustomer() {
+  return bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = customerName;
+
+function changeLeastFavoriteCustomer() {
+  return leastFavoriteCustomer = "bob";
+}
